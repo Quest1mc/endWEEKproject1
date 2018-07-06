b3abeb9d824b533a7963e01465e5798f3bec6579
Stefan Quest <stefan.quest@gmail.com>
2018-07-06 19:47:54 +0200

    game logic functions being added

--- BB-HW-single-day/YOUR_JAVASCRIPT_HERE.js
+++ BB-HW-single-day/YOUR_JAVASCRIPT_HERE.js
@@ -1,1 +1,17 @@
 // Write your JS here
+let hero ={
+    name: "Hancock", 
+    superStrength: yes,
+    inventory: [],
+    health : 10,
+    weapon: {
+        type:"Hammer",
+        damage: 2,
+
+
+    }
+
+}
+expect(sinon.match.defined.test(rest)).to.equal(true)
+expect(sinon.match.defined.test(pickUpItem)).to.equal(true)
+expect(sinon.match.defined.test(equipWeapon)).to.equal(true)
